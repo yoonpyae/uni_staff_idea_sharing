3e63c9dcbcae9e5579c037fb2043a396929d0f69
ypeain <yoonpyae26@gmail.com>
2026-02-12 20:56:55 +0630

    Update app.component.ts

--- uni-staff-idea-sharing-client/src/app/app.component.ts
+++ uni-staff-idea-sharing-client/src/app/app.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
+import { filter, map, startWith } from 'rxjs';
+import { TitleService } from './Shared/services/title.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, ToastModule],
+  providers: [TitleService],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'uni-staff-idea-sharing-client';
+
+
+  constructor(
+    private titleService: TitleService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.router.events
+    .pipe(
+      filter((event) => event instanceof NavigationEnd),
+      startWith(true), // Ensure execution on refresh
+      map(() => this.activatedRoute),
+      map((route) => {
+        const titles: string[] = [];
+        while (route) {
+          if (route.snapshot.data['title']) {
+            titles.unshift(route.snapshot.data['title']);
+          }
+          route = route.firstChild!;
+        }
+        return titles;
+      })
+    )
+    .subscribe((titles) => {
+      this.titleService.setTitle(titles);
+    });
+  }
 }
