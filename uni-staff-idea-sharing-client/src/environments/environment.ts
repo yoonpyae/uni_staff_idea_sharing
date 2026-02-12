cca3c9a40883149eef9f7508ffa5bfc5392fec68
ypeain <yoonpyae26@gmail.com>
2026-02-12 20:57:23 +0630

    Create environment.ts

---
+++ uni-staff-idea-sharing-client/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  docker: false,
+
+  main_url: 'https://localhost:7126/api',
+  web_url: 'https://localhost:7126',
+
+  debug: true,
+
+  default_pagination: [10, 20, 30, 50, 100],
+
+  default_toastKey: "globalMessage"
+};
