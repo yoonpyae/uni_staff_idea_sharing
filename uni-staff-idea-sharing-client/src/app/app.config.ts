32697e4def5b811b358e2480c132c34a176b450c
ypeain <yoonpyae26@gmail.com>
2026-02-15 20:50:16 +0630

    Update app.config.ts

--- uni-staff-idea-sharing-client/src/app/app.config.ts
+++ uni-staff-idea-sharing-client/src/app/app.config.ts
@@ -1,9 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+     provideAnimations(),
+      providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
+     provideRouter(routes), 
+     provideClientHydration(withEventReplay())]
 };
