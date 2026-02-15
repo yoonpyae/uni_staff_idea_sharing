5537bb58b19f7d29171e1e4395a3ed010af3ad2f
ypeain <yoonpyae26@gmail.com>
2026-02-15 20:50:20 +0630

    Create app.menu.ts

---
+++ uni-staff-idea-sharing-client/src/app/app.menu.ts
@@ -0,0 +1,54 @@
+export interface MenuItem {
+  label: string;
+  icon?: string;
+  routerLink?: string[];
+  items?: MenuItem[];
+}
+
+export const NAVIGATION_MENU: Readonly<MenuItem[]> = [
+  {
+    label: 'Home',
+    items: [
+      {
+        label: 'Dashboard',
+        icon: 'pi pi-fw pi-chart-pie',
+        routerLink: ['/dashboard'],
+      },
+    ],
+  },
+  {
+    label: 'Management',
+    items: [
+      {
+        label: 'Departments',
+        icon: 'pi pi-fw pi-building',
+        routerLink: ['/departments'],
+      },
+      {
+        label: 'Staff Accounts',
+        icon: 'pi pi-fw pi-users',
+        routerLink: ['/staff-accounts'],
+      },
+      {
+        label: 'Submit Ideas',
+        icon: 'pi pi-fw pi-lightbulb',
+        routerLink: ['/submit-ideas'],
+      },
+      {
+        label: 'Closure Dates',
+        icon: 'pi pi-fw pi-calendar',
+        routerLink: ['/closure-dates'],
+      },
+    ],
+  },
+  {
+    label: 'Settings',
+    items: [
+      {
+        label: 'System Settings',
+        icon: 'pi pi-fw pi-cog',
+        routerLink: ['/system-settings'],
+      },
+    ],
+  },
+];
