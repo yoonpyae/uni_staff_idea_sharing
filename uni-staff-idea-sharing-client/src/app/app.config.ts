e3c90288aad937e8e81d8d30b2acd1c337e3265c
ypeain <yoonpyae26@gmail.com>
2026-02-17 21:35:46 +0630

    Update app.config.ts

--- uni-staff-idea-sharing-client/src/app/app.config.ts
+++ uni-staff-idea-sharing-client/src/app/app.config.ts
@@ -1,15 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
+import { MessageService } from 'primeng/api';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-     provideAnimations(),
-      providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
-     provideRouter(routes), 
-     provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimations(),
+    providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule),
+    provideClientHydration(withEventReplay()),
+    MessageService
+  ]
 };
