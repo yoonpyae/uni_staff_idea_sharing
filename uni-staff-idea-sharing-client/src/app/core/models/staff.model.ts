8b798e9551fe18525e933808c6142930f472739d
ypeain <yoonpyae26@gmail.com>
2026-02-17 21:36:40 +0630

    Create staff.model.ts

---
+++ uni-staff-idea-sharing-client/src/app/core/models/staff.model.ts
@@ -0,0 +1,14 @@
+export interface StaffModel {
+    staffID: number;
+    staffName: string;
+    staffPhNo: string;
+    staffEmail: string;
+    staffDOB: string;
+    staffAddress: string;
+    staffProfile: string;
+    termsAccepted: number;
+    termsAcceptedDate: null;
+    createdDateTime: string;
+    departmentID: number;
+    roleID: number;
+}
