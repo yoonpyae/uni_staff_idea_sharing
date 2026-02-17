4d2bd3d6af08e98c0fc43fb07c073660c461f26b
ypeain <yoonpyae26@gmail.com>
2026-02-17 21:36:44 +0630

    Create auth.service.ts

---
+++ uni-staff-idea-sharing-client/src/app/core/services/auth.service.ts
@@ -0,0 +1,114 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable, tap } from 'rxjs';
+import { RootModel } from '../models/root.model';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private readonly tokenKey = 'access_token';
+  private readonly refreshTokenKey = 'refresh_token';
+
+  constructor(
+    private http: HttpClient,
+    private cookieService: CookieService
+  ) { }
+
+  login(email: string, password: string): Observable<RootModel> {
+    const url = `${environment.main_url}/staff/login`;
+
+    return this.http.post<RootModel>(url, {
+      staffEmail: email,
+      staffPassword: password
+    }).pipe(
+      tap(res => {
+        if (res.success) {
+          this.storeSession(res);
+        }
+      })
+    );
+  }
+
+  private storeSession(res: RootModel): void {
+    const data: any = res?.data || {};
+
+    const accessToken = data.token || data.access_token;
+    const refreshToken = data.refresh_token || data.refreshToken;
+
+    if (accessToken) {
+      localStorage.setItem(this.tokenKey, accessToken);
+    }
+
+    if (refreshToken) {
+      localStorage.setItem(this.refreshTokenKey, refreshToken);
+    }
+
+    const staff = data.staffs || data.user || data;
+
+    if (staff) {
+      const id = staff.staffID || staff.id;
+      const name = staff.staffName || staff.name;
+      const role = staff.roleID || staff.role;
+      const profilePicture = staff.staffProfile;
+
+      if (id) this.cookieService.set('staffID', String(id));
+      if (name) this.cookieService.set('staffName', String(name));
+      if (role) this.cookieService.set('roleID', String(role));
+      if (profilePicture) this.cookieService.set('staffProfile', String(profilePicture));
+    }
+
+    this.cookieService.set('authorized_status', 'true');
+  }
+
+  me(): Observable<RootModel> {
+    return this.http.get<RootModel>(`${environment.main_url}/staff/me`);
+  }
+
+  refreshToken(): Observable<RootModel> {
+    return this.http.post<RootModel>(`${environment.main_url}/staff/refresh`, {})
+      .pipe(
+        tap(res => {
+          if (res.success && res.data?.token) {
+            localStorage.setItem(this.tokenKey, res.data.token);
+          }
+        })
+      );
+  }
+
+  logout(): Observable<RootModel> {
+    const token = this.getToken();
+
+    const headers = token
+      ? new HttpHeaders().set('Authorization', `Bearer ${token}`)
+      : undefined;
+
+    return this.http.post<RootModel>(
+      `${environment.main_url}/staff/logout`,
+      {},
+      { headers }
+    );
+  }
+
+  logoutForce(): void {
+    this.cookieService.delete('authorized_status');
+    this.cookieService.delete('staffID');
+    this.cookieService.delete('staffName');
+    this.cookieService.delete('roleID');
+    this.cookieService.delete('staffProfile');
+
+    localStorage.removeItem(this.tokenKey);
+    localStorage.removeItem(this.refreshTokenKey);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+}
