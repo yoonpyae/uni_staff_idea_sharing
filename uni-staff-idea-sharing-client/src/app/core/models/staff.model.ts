d75a08616bd46cf45705c1fb43c3c6ea091eb839
ypeain <yoonpyae26@gmail.com>
2026-02-21 12:18:31 +0630

    Update staff.model.ts

--- uni-staff-idea-sharing-client/src/app/core/models/staff.model.ts
+++ uni-staff-idea-sharing-client/src/app/core/models/staff.model.ts
@@ -12,3 +12,34 @@ export interface StaffModel {
     departmentID: number;
     roleID: number;
 }
+
+export interface ViewStaffModel {
+    staffID: number;
+    staffName: string;
+    staffPhNo: string;
+    staffEmail: string;
+    staffDOB: string;
+    staffAddress: string;
+    staffProfile: string;
+    termsAccepted: number;
+    termsAcceptedDate: null;
+    createdDateTime: string;
+    departmentID: number;
+    roleID: number;
+    department: Department;
+    role: Role;
+}
+
+export interface Role {
+    roleID: number;
+    roleName: string;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface Department {
+    departmentID: number;
+    departmentName: string;
+    created_at: string;
+    updated_at: string;
+}
