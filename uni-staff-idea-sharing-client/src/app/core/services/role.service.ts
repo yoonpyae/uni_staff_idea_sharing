7f95d9ed9e33e45cd1b9ca36870e10399f9c102d
ypeain <yoonpyae26@gmail.com>
2026-02-21 13:29:25 +0630

    Create role.service.ts

---
+++ uni-staff-idea-sharing-client/src/app/core/services/role.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { RootModel } from '../models/root.model';
+import { RoleModel } from '../models/role.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  get(): Observable<RootModel> {
+    return this.httpClient.get<RootModel>(`${environment.main_url}/roles`);
+  }
+
+  getById(id: number): Observable<RootModel> {
+    return this.httpClient.get<RootModel>(`${environment.main_url}/roles/${id}`);
+  }
+
+  create(model: RoleModel): Observable<RootModel> {
+    return this.httpClient.post<RootModel>(`${environment.main_url}/roles`, model);
+  }
+
+  update(id: number, model: RoleModel): Observable<RootModel> {
+    return this.httpClient.post<RootModel>(`${environment.main_url}/roles/${id}`, model);
+  }
+
+  delete(id: number): Observable<RootModel> {
+    return this.httpClient.delete<RootModel>(`${environment.main_url}/roles/${id}`);
+  }
+}
