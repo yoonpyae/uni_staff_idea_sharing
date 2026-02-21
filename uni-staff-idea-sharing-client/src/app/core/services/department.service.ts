177157602a26af250acf749fd23f1eaea366ca6a
ypeain <yoonpyae26@gmail.com>
2026-02-21 13:29:17 +0630

    Create department.service.ts

---
+++ uni-staff-idea-sharing-client/src/app/core/services/department.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { RootModel } from '../models/root.model';
+import { DepartmentModel } from '../models/department.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  get(): Observable<RootModel> {
+    return this.httpClient.get<RootModel>(`${environment.main_url}/departments`);
+  }
+
+  getById(id: number): Observable<RootModel> {
+    return this.httpClient.get<RootModel>(`${environment.main_url}/departments/${id}`);
+  }
+
+  create(model: DepartmentModel): Observable<RootModel> {
+    return this.httpClient.post<RootModel>(`${environment.main_url}/departments`, model);
+  }
+
+  update(id: number, model: DepartmentModel): Observable<RootModel> {
+    return this.httpClient.post<RootModel>(`${environment.main_url}/departments/${id}`, model);
+  }
+
+  delete(id: number): Observable<RootModel> {
+    return this.httpClient.delete<RootModel>(`${environment.main_url}/departments/${id}`);
+  }
+}
