c91e0925aa35c95d5de2a412ebabf671896a804c
ypeain <yoonpyae26@gmail.com>
2026-03-01 19:25:12 +0630

    Create permission.component.ts

---
+++ uni-staff-idea-sharing-client/src/app/pages/role/permission/permission.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-permission',
+  imports: [],
+  templateUrl: './permission.component.html',
+  styleUrl: './permission.component.scss'
+})
+export class PermissionComponent {
+
+}
