c8d05def8c54af569a32605ea1254027a65c4fad
ypeain <yoonpyae26@gmail.com>
2026-03-01 22:05:31 +0630

    Create closer-date.component.ts

---
+++ uni-staff-idea-sharing-client/src/app/pages/closer-date/closer-date.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-closer-date',
+  imports: [],
+  templateUrl: './closer-date.component.html',
+  styleUrl: './closer-date.component.scss'
+})
+export class CloserDateComponent {
+
+}
