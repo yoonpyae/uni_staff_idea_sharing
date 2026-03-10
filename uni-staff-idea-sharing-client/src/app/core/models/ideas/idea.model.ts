6b1a968d0e3076c664ebb75673e58cee2bd62cf3
ypeain <yoonpyae26@gmail.com>
2026-03-10 22:18:39 +0630

    Update idea.model.ts

--- uni-staff-idea-sharing-client/src/app/core/models/ideas/idea.model.ts
+++ uni-staff-idea-sharing-client/src/app/core/models/ideas/idea.model.ts
@@ -1,1 +1,26 @@
+import { CategoryModel } from "../category.model";
+import { StaffModel } from "../staff.model";
+import { CommentModel } from "./comment.model";
+import { VoteModel } from "./vote.model";
+
+export interface IdeaModel {
+  ideaID: number;
+  title: string;
+  description: string;
+  isAnonymous: boolean | number;
+  staffID: number;
+  settingID: number;
+  status: string;
+  created_at?: string;
+  updated_at?: string;
+  
+  staff?: StaffModel;
+  categories?: CategoryModel[];
+  comments?: CommentModel[];
+  votes?: VoteModel[];
+  
   likesCount?: number;
+  unlikesCount?: number;
+  commentsCount?: number;
+  viewsCount?: number; // Note: Add this to your backend if you want real view tracking
+}
