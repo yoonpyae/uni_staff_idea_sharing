5c17129db7f2f4ea382cbafab9d1d28751c95163
ypeain <yoonpyae26@gmail.com>
2026-03-10 22:18:49 +0630

    Create comment.service.ts

---
+++ uni-staff-idea-sharing-client/src/app/core/services/ideas/comment.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { RootModel } from '../../models/root.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private http: HttpClient) { }
+
+  get(): Observable<RootModel> {
+    return this.http.get<RootModel>(`${environment.main_url}/comments`);
+  }
+
+  create(model: any): Observable<RootModel> {
+    return this.http.post<RootModel>(`${environment.main_url}/comments`, model);
+  }
+
+  delete(id: number): Observable<RootModel> {
+    return this.http.delete<RootModel>(`${environment.main_url}/comments/${id}`);
+  }
+}
