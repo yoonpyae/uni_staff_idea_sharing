355f1c69f4d973defc95e59e89071efa915c5ea7
ypeain <yoonpyae26@gmail.com>
2026-03-10 22:19:19 +0630

    Create vote.service.ts

---
+++ uni-staff-idea-sharing-client/src/app/core/services/ideas/vote.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { RootModel } from '../../models/root.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VoteService {
+
+  constructor(private http: HttpClient) { }
+
+  store(model: { voteType: string, staffID: number, ideaID: number }): Observable<RootModel> {
+    return this.http.post<RootModel>(`${environment.main_url}/votes`, model);
+  }
+
+  update(id: number, model: any): Observable<RootModel> {
+    return this.http.put<RootModel>(`${environment.main_url}/votes/${id}`, model);
+  }
+
+  delete(id: number): Observable<RootModel> {
+    return this.http.delete<RootModel>(`${environment.main_url}/votes/${id}`);
+  }
+}
+
