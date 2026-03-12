e0686b55d2c11a91badc5e59589067d423f6cd03
ypeain <yoonpyae26@gmail.com>
2026-03-12 17:40:18 +0630

    Update document.model.ts

--- uni-staff-idea-sharing-client/src/app/core/models/ideas/document.model.ts
+++ uni-staff-idea-sharing-client/src/app/core/models/ideas/document.model.ts
@@ -1,6 +1,9 @@
 export interface DocumentModel {
   documentID: number;
   docPath: string;
+  fileType: string;
+  fileSize: number;
+  isHidden: boolean | number;
   ideaID: number;
   created_at?: string;
   updated_at?: string;
