f1c46817c3b97d383ed819c98b029f039af112e3
ypeain <yoonpyae26@gmail.com>
2026-03-23 16:00:45 +0630

    file changes

---
+++ uni-staff-idea-sharing-client/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  docker: false,
+
+  main_url: 'http://127.0.0.1:8000/api',
+  web_url: 'https://api.uniidea.site',
+
+  debug: true,
+
+  default_pagination: [10, 20, 30, 50, 100],
+
+  default_toastKey: "globalMessage"
+};
