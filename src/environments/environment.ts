bc59ffa2c051aeeb8e52a24e9992123ca479d91e
ypeain <yoonpyae26@gmail.com>
2026-04-18 10:49:15 +0630

    file location changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  docker: false,
+
+  base_url: 'http://127.0.0.1:8000',
+  main_url: 'http://127.0.0.1:8000/api',
+
+  // base_url: 'https://api.uniidea.site',
+  // main_url: 'https://api.uniidea.site/api',
+
+  debug: true,
+
+  default_pagination: [10, 20, 30, 50, 100],
+
+  default_toastKey: "globalMessage"
+};
