10cb41466e5be99931829a0e8d145d16547c3f28
ypeain <yoonpyae26@gmail.com>
2026-04-18 10:51:28 +0630

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   docker: false,
 
-  base_url: 'http://127.0.0.1:8000',
-  main_url: 'http://127.0.0.1:8000/api',
+  // base_url: 'http://127.0.0.1:8000',
+  // main_url: 'http://127.0.0.1:8000/api',
 
-  // base_url: 'https://api.uniidea.site',
-  // main_url: 'https://api.uniidea.site/api',
+  base_url: 'https://api.uniidea.site',
+  main_url: 'https://api.uniidea.site/api',
 
   debug: true,
 
